e80e9de447c9eed12d5efe522f25596cf670b416
Luis Juarez <luis4juarez@gmail.com>
2022-08-25 19:25:14 -0500

    mostrar datos

---
+++ Prueba/Index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Prueba.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Prueba</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView runat="server" ID="gvtabla" class="table table-borderless table-hover">
+                <Columns>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Prueba/Index.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prueba
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        readonly SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarTabla();
+        }
+        void CargarTabla()
+        {
+            SqlCommand cmd = new SqlCommand("sp_load", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvtabla.DataSource = dt;
+            gvtabla.DataBind();
+            con.Close();
+        }
+    }
+}
